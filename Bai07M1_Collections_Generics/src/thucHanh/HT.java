763079956f092800d3a76fc103b114b3eb802c3e
quocbaole <quocbao1997la@gmail.com>
2022-07-01 18:53:37 +0700

    lop dung chung

--- Bai07M1_Collections_Generics/src/thucHanh/HT.java
+++ Bai07M1_Collections_Generics/src/thucHanh/HT.java
@@ -1,7 +1,40 @@
 package thucHanh;
 
-public class HT {
-	private int bk;
-	
+public class HT<T> {
+	private T bk;
 
+	public HT(T bk) {
+		super();
+		this.bk = bk;
+	}
+
+	public HT() {
+
+	}
+
+	public double chuVi() {
+		double r = Double.parseDouble(bk.toString());
+		return r * 2 * Math.PI;
+	}
+
+	public double dientich() {
+		double r = Double.parseDouble(bk.toString());
+		return r * r * Math.PI;
+	}
+
+	public String xuat() {
+		// TODO Auto-generated method stub
+		return String.format("Chu vi :%.2f, Dien tich: %.2f", this.chuVi(), this.dientich());
+	}
+
+	public static void main(String[] args) {
+		HT<Integer> ht = new HT<Integer>(5);
+		System.out.println(ht.xuat());
+		HT<Float> ht1 = new HT<Float>(6.5f);
+		System.out.println(ht1.xuat());
+		HT<Double> ht2 = new HT<Double>(8.5);
+		System.out.println(ht2.xuat());
+		
+		HT ht5 = new HT(5);
+	}
 }
